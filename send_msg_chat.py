f05081d30389243aeec0895ad28b1da20acf2230
svillors <tntzator9@gmail.com>
2025-09-21 19:09:44 +0200

    Add CLI options to sender script

--- send_msg_chat.py
+++ send_msg_chat.py
@@ -1,5 +1,6 @@
 import logging
 import asyncio
+import argparse
 import json
 
 
@@ -14,19 +15,38 @@ async def log_msg(reader):
     return text
 
 
-async def send_message():
-    reader, writer = await asyncio.open_connection('minechat.dvmn.org', 5050)
+async def send_message(host, port, token, msg):
+    reader, writer = await asyncio.open_connection(host, port)
     await log_msg(reader)
-    writer.write('hash\n'.encode())
+    writer.write(f'{token}\n'.encode())
     await writer.drain()
     response = await log_msg(reader)
     if json.loads(response) is None:
         logger.error('Unknown token. Check it or register again.')
         return
-    writer.write('qweqweqweqweqwe\n\n'.encode())
+    writer.write(f'{msg}\n\n'.encode())
     await writer.drain()
     await log_msg(reader)
 
 
 if __name__ == '__main__':
-    asyncio.run(send_message())
+    parser = argparse.ArgumentParser(description='async chat message sender')
+    parser.add_argument(
+        '-H', '--host',
+        help='ip or link to host'
+    )
+    parser.add_argument(
+        '-p', '--port',
+        help='port for connnection',
+        type=int
+    )
+    parser.add_argument(
+        '-t', '--token',
+        help='user token',
+    )
+    parser.add_argument(
+        '-m', '--message',
+        help='message to send',
+    )
+    args = parser.parse_args()
+    asyncio.run(send_message(args.host, args.port, args.token, args.message))
